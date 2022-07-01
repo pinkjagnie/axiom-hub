abeccb0170d0f4f1a45a91c1d7188f55ec8276a2
OlgaKrzysztofik <olga.krzysztofik@gmail.com>
2022-07-02 01:17:08 +0200

    Extract from About page Team

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,9 @@
 import React from "react";
 
+import Team from "./Team";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTwitter, faGithub, faTelegram, faPaypal, faPatreon } from "@fortawesome/free-brands-svg-icons";
+import { faTelegram } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 import styles from "./about.module.css";
@@ -36,61 +38,8 @@ const About = () => {
           </div>
         </div>
 
-        <div className={styles.team}>
-          <p className={styles.meetUs}>Let's meet!</p>
-          <div className={styles.members}>
-
-            <div className={styles.teamMember}>
-              <div className={styles.name}>
-                <div className={styles.profilePicture}>
-                  <img src="https://avatars.githubusercontent.com/u/103602667?v=4" alt="miniature profile picture" />
-                </div>
-                <div className={styles.welcome}>
-                  <p>@ojszymon</p>
-                  <p>Lead developer</p>
-                </div>
-              </div>
-              <p className={styles.aboutMember}>Go lang | Java | Very nice sheep | Spoiler: already has a wife</p>
-            </div>
-            <div className={styles.teamMember}>
-              <div className={styles.name}>
-                <div className={styles.profilePicture}>
-                  <img src="https://avatars.githubusercontent.com/u/97997260?v=4" alt="miniature profile picture" />
-                </div>
-                <div className={styles.welcome}>
-                  <p>@pinkjagnie</p>
-                  <p>Frontend developer</p>
-                </div>
-              </div>
-              <p className={styles.aboutMember}>Just some React and CSS. Here also as a graphic artist and UI designer</p>
-            </div>
-            <div className={styles.teamMember}>
-              <div className={styles.name}>
-                <div className={styles.profilePicture}>
-                  <img src="https://avatars.githubusercontent.com/u/103602667?v=4" alt="miniature profile picture" />
-                </div>
-                <div className={styles.welcome}>
-                  <p>@ojszymon</p>
-                  <p>Lead developer</p>
-                </div>
-              </div>
-              <p className={styles.aboutMember}>Go lang | Java | Very nice sheep | Spoiler: already has a wife</p>
-            </div>
-            <div className={styles.teamMember}>
-              <div className={styles.name}>
-                <div className={styles.profilePicture}>
-                  <img src="https://avatars.githubusercontent.com/u/97997260?v=4" alt="miniature profile picture" />
-                </div>
-                <div className={styles.welcome}>
-                  <p>@pinkjagnie</p>
-                  <p>Frontend developer</p>
-                </div>
-              </div>
-              <p className={styles.aboutMember}>Just some React and CSS. Here also as a graphic artist and UI designer</p>
-            </div>
-          </div>
-        </div>
-
+        <Team/>
+          
       </div>
   )
 };
