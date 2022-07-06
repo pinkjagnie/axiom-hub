e372af9039ece489cee268264ae135adee5d5c40
OlgaKrzysztofik <olga.krzysztofik@gmail.com>
2022-07-06 20:05:20 +0200

    In AddSingleApp add form + styling all to 1366px

--- src/pages/AddSingleApp.js
+++ src/pages/AddSingleApp.js
@@ -5,8 +5,24 @@ import styles from "../components/addSingleApp.module.css";
 const AddSingleApp = () => {
   return(
     <div className={styles.addSingleAppContainer}>
-      <h1>Did not find what you were looking for?</h1>
-      <h2>Fill in the form and we will add this application to the database</h2>
+
+      <div className={styles.titleSection}>
+        <p>Did not find what you were looking for?</p>
+        <p>Fill in the form and we will add this application to the database</p>
+      </div>
+
+      <form className={styles.form}>
+        <div className={styles.control}>
+          <label htmlFor="name">Name of the application package</label>
+          <input type="text" id="name"/>
+        </div>
+        <div className={styles.control}>
+          <label htmlFor="url">Link to the package in Google Play</label>
+          <input type="url" id="url"/>
+        </div>
+        <button>Submit</button>
+      </form>
+
     </div>
   )
 };
