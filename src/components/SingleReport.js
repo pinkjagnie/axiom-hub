6e2a0c36e5f7423a66498e871032ef9af5f2ff87
OlgaKrzysztofik <olga.krzysztofik@gmail.com>
2022-07-09 23:58:41 +0200

    Styled SingleReport to all widths

--- src/components/SingleReport.js
+++ src/components/SingleReport.js
@@ -15,17 +15,28 @@ const SingleReport = (props) => {
       <div className={styles.infoText}>
         <div className={styles.appTitle}>
           <img src={hubIcon} alt="app icon"/>
-          <h1>{props.data.report.app_name}</h1>
-          <p>{props.data.report.package_name}</p>
+          {/* <img src={props.data.report.app_icon} alt="app icon" /> */}
+          <div className={styles.appNames}>
+            <h1>{props.data.report.app_name}</h1>
+            <p>{props.data.report.package_name}</p>
+            <p>version: {props.data.report.app_version}</p>
+          </div>
         </div>
-        <p>version: {props.data.report.app_version}</p>
         <h2><FaClipboardList className={styles.icon}/> Scores</h2>
         <p>Perrmisions score: {props.data.report.perrmisions_score}</p>
         <p>Privacy score: {props.data.report.privacy_score}</p>
         <p>Rules score: {props.data.report.rules_score}</p>
         <h2><MdReadMore className={styles.icon}/> More to know</h2>
-        {/* <p>App privacy report link: <a href={value.app_privacy_report_link} target="_blank" rel="noopener noreferrer">here</a></p>
-          <p>App rules report link: <a href={value.app_rules_report_link} target="_blank" rel="noopener noreferrer">here</a></p> */}
+        <div className={styles.numberContainer}>
+          <div className={styles.number}><p>{props.data.report.trackers_count}</p></div>
+          <p className={styles.numberName}>Trackers</p>
+        </div>
+          {props.data.report.trackers.map((tracker) => <div className={styles.numberList}><p key={tracker.ID}>{tracker.tracker_name}</p></div>)}
+        <div className={styles.numberContainer}>
+          <div className={styles.number}><p>{props.data.report.permissions_count}</p></div>
+          <p className={styles.numberName}>Permissions</p>
+        </div>
+          {props.data.report.permissions.map((permission) => <div className={styles.numberList}><p key={permission.ID}>{permission.permission_name}</p></div>)}
       </div>
       <div className={styles.infoLegend}>
         <h3><FaQuestionCircle className={styles.icon}/> Legend</h3>
