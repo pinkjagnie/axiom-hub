44e4aed683de40d1eaca541c1a9ff2172c6bf87d
OlgaKrzysztofik <olga.krzysztofik@gmail.com>
2022-07-12 22:11:30 +0200

    In AddSingleApp connect with API, add ninja image

--- src/pages/AddSingleApp.js
+++ src/pages/AddSingleApp.js
@@ -1,26 +1,16 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import useInput from "../hooks/use-input";
 
 import ErrorModal from "../components/Modal/ErrorModal";
 
+import ninja from "../img/ninja_800-800.png";
 import styles from "../components/addSingleApp.module.css";
 
-const isNotEmpty = (value) => value.trim() !== '';
 const isUrl = (value) => value.includes('play.google.com/');
 
 const AddSingleApp = () => {
   const [modalIsShown, setModalIsShown] = useState(false);
   const [errorMessage, setErrorMessage] = useState();
-
-  const {
-    value: nameValue,
-    isValid: nameIsValid,
-    hasError: nameHasError,
-    valueChangeHandler: nameChangeHandler,
-    inputBlurHandler: nameBlurHandler,
-    reset: resetName,
-  } = useInput(isNotEmpty);
 
   const {
     value: urlValue,
@@ -31,20 +21,24 @@ const AddSingleApp = () => {
     reset: resetUrl,
   } = useInput(isUrl);
 
+  const urlName = urlValue.slice(
+    urlValue.indexOf('=') + 1,
+    urlValue.lastIndexOf('&h'),
+  );
+
   let formIsValid = false;
 
   const hideModalHandler = () => {
     setModalIsShown(false);
   };
 
-  async function addApp(nameValue, urlValue) {
+  async function addApp(urlName) {
     let errorModalMessage = '';
-    const response = await fetch('https://axiomos-hub-default-rtdb.europe-west1.firebasedatabase.app/addNewApp.json', {
+    const response = await fetch('https://test.axiomos.pl/api/reports/request', {
       method: 'POST',
-      // mode: 'cors',
+      mode: 'cors',
       body: JSON.stringify({
-        name: nameValue,
-        url: urlValue
+        "package_name": urlName
       }),
       headers: {
         'Content-Type': 'application/json'
@@ -68,50 +62,38 @@ const AddSingleApp = () => {
   const submitHandler = (event) => {
     event.preventDefault();
 
-    if (nameIsValid && urlIsValid) {
+    if (urlIsValid) {
       formIsValid = true;
     } else {
       return;
     }
 
-    console.log(nameValue, urlValue);
+    addApp(urlName);
 
-    addApp(nameValue, urlValue);
-
-    resetName();
     resetUrl();
     formIsValid = false;
   };
-
-  const nameLabelClass = nameHasError ? {color: '#9b0000'} : undefined;
-  const nameInputClass = nameHasError ? {border: '2px solid #9b0000'} : undefined;
 
   const urlLabelClass = urlHasError ? {color: '#9b0000'} : undefined;
   const urlInputClass = urlHasError ? {border: '2px solid #9b0000'} : undefined;
 
   return(
     <div className={styles.addSingleAppContainer}>
-
       <div className={styles.titleSection}>
         <p>Did not find what you were looking for?</p>
         <p>Fill in the form and we will add this application to the database</p>
       </div>
-
+      <div className={styles.ninjaIcon}>
+        <img src={ninja} alt="ninja"/>
+      </div>
       {modalIsShown && <ErrorModal onClose={hideModalHandler} errorMessage={errorMessage} />}
       <form className={styles.form} onSubmit={submitHandler}>
-        <div className={styles.control}>
-          <label htmlFor="name" style={nameLabelClass}>Name of the application package</label>
-          <input type="text" id="name" value={nameValue} onChange={nameChangeHandler} onBlur={nameBlurHandler} style={nameInputClass} />
-        </div>
         <div className={styles.control}>
           <label htmlFor="url" style={urlLabelClass}>Link to the package in Google Play</label>
           <input type="url" id="url" value={urlValue} onChange={urlChangeHandler} onBlur={urlBlurHandler} style={urlInputClass} />
         </div>
         <button>Submit</button>
       </form>
-      <div className={styles.linkContainer}>
-        <Link to='/reports' className={styles.link}>Go to search for Reports</Link>
-      </div>
     </div>
   )
 };
